3bab603a2adc39a0897285337691ac0922fe9ce0
Luciano Bampa <lucbam2@gmail.com>
2025-07-19 19:24:43 -0300

    Atualização do README como trabalhar com o shell do Django

--- contact/admin.py
+++ contact/admin.py
@@ -9,7 +9,7 @@ class ContactAdmin(admin.ModelAdmin):
     # ordering = 'id', # ordem crescente
     ordering = '-id',  # ordem decrescente
     # list_filter = 'created_date',
-    search_fields = 'id', 'first-name', 'last_name',
+    search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
