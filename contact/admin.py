bc65cc016ee983f2956d5b3eb788a6e440ba715d
Luciano Bampa <lucbam2@gmail.com>
2025-07-19 11:49:08 -0300

    Customizando as opcoes admin (ModelAdmin)

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    # ordering = 'id', # ordem crescente
+    ordering = '-id',  # ordem decrescente
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first-name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
