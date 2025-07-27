4e8bcdd5e13b371b9b3ab9ae3141c63aa2b0d176
LucianoBampa <lucbam2@gmail.com>
2025-07-26 21:44:08 -0300

    Correção de url

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,7 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<search/', views.search, name='search'),
+    path('search/', views.search, name='search'),
 
 
     # contact (CRUD)
